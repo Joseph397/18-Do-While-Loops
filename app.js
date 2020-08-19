866915c8828e2a263bba33002485f9953d20e198
Joseph397 <gramkracker88@gmail.com>
2020-08-19 14:04:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Loops 
+// repeatidly run a block of code while condition is true
+// do while Loop
+// code block first, condition second 
+// runs at least once
+
+let money = 12;
+
+do {
+    console.log('you have ' + money + ' dollars');
+    money++;
+} while(money < 10);
